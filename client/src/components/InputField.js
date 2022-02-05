ff876bd27bc089d845bd7b08ab7bf08f0d8e05ab
Shavy <shavypeynado@gmail.com>
2022-02-05 18:33:16 +0000

    creating a form for sign up component

---
+++ client/src/components/InputField.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const InputField = (props) => {
+    return (
+      <>
+      <input className="form" placeholder={props.placeholder}></input>
+      </>  
+    )
+}
+
+export default InputField;
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,8 +1,22 @@
+import React from "react";
+import InputField from "./InputField";
+
+
 const SignUp = () => {
     return (
+        <>
         <div className="container">
 
         </div>
+        <div>
+            <form>
+                <InputField placeholder="Full name" type="text" />
+                <InputField placeholder=""/>
+                <InputField/>
+                <InputField/>
+            </form>
+        </div>
+        </>
     );
 }
  
