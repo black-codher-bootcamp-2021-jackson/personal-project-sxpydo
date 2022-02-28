9d470a360f315e478162f90fe96cec7dfad1391c
Shavy <shavypeynado@gmail.com>
2022-02-28 12:18:45 +0000

    resized the maps and added longitude and latitude with coords to Leeds

--- client/src/components/features/Map.js
+++ client/src/components/features/Map.js
@@ -5,40 +5,41 @@ import '../../styles/Maps.css';
 mapboxgl.accessToken =
   'pk.eyJ1Ijoic3hweWRvIiwiYSI6ImNrem43ejh0aDA1cmoyb3F1bGs5bWkxd2UifQ._jXxhAY9xq4vxDz_gDCtgA';
 
-const Maps = () => {
-  const mapContainerRef = useRef(null);
+  export default function Maps() {
+      const mapContainer = useRef(null);
+      const map = useRef(null);
+      const [lng, setLng] = useState(-1.548567);
+      const [lat, setLat] = useState(53.80);
+      const [zoom, setZoom] = useState(9);
 
-  const [lng, setLng] = useState(5);
-  const [lat, setLat] = useState(34);
-  const [zoom, setZoom] = useState(1.5);
+      useEffect(() => {
+          if (map.current) return; // initialize map only once
+          map.current = new mapboxgl.Map({
+          container: mapContainer.current,
+          style: 'mapbox://styles/mapbox/outdoors-v11',
+          center: [lng, lat],
+          zoom: zoom
+          });
+        });
 
-  // Initialize map when component mounts
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      style: 'mapbox://styles/mapbox/outdoors-v11',
-      center: [lng, lat],
-      zoom: zoom
-    });
+      useEffect(() => {
+          if (!map.current) return; // wait for map to initialize
+          map.current.on('move', () => {
+          setLng(map.current.getCenter().lng.toFixed(4));
+          setLat(map.current.getCenter().lat.toFixed(4));
+          setZoom(map.current.getZoom().toFixed(2));
+          });
+        });
 
-    // Add navigation control (the +/- zoom buttons)
-    map.addControl(new mapboxgl.NavigationControl(), 'top-right');
 
-    map.on('move', () => {
-      setLng(map.getCenter().lng.toFixed(4));
-      setLat(map.getCenter().lat.toFixed(4));
-      setZoom(map.getZoom().toFixed(2));
-    });
+    return (
+              <div>
+              <div className="sidebar">
+              Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+              </div>
+              <div ref={mapContainer} className="map-container" />
+              </div>
+            );
 
-    // Clean up on unmount
-    return () => map.remove();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  return (
-    <>
-      <div className='map-container' ref={mapContainerRef} />
-    </>
-  );
-};
-
-export default Maps;
+    }
